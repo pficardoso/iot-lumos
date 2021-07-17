0526cd6641a05a5219888dbbcddea8ff4de103c4
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-17 17:57:08 +0100

    Created class LedControler - initial commit

---
+++ src/lumos/LedControler/LedControler.py
@@ -0,0 +1,30 @@
+class LedController:
+    """"""
+
+    def __init__(self,):
+        """Constructor for """
+
+    """
+    Setters/Loaders
+    """
+
+    """
+    Getters
+    """
+
+    """
+    Workers
+    """
+
+    """
+    Boolean methods
+    """
+
+    """
+    Checkers
+    """
+
+    """
+    Util methods / Static methods
+    """
+
