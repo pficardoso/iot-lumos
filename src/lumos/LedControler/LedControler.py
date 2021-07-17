bf4c724c2be25c9b3bf303aac69630fe20df4280
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-17 17:59:31 +0100

    Added logger to class LedController.py - closes #6

--- src/lumos/LedControler/LedControler.py
+++ src/lumos/LedControler/LedControler.py
@@ -1,8 +1,14 @@
+import logging
+import lumos.logger
+
+logger = logging.getLogger("led_controller")
+
 class LedController:
     """"""
 
     def __init__(self,):
         """Constructor for """
+        logger.info(f"Creating object of LedControler")
 
     """
     Setters/Loaders
