6a6c63185e7bb85df03fc82bac6d4d816fdf15f5
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-24 18:58:46 +0100

    LedController: creted endpoint in WebService class - the endpoint receives requests from listeners - closes #13

--- src/lumos/LedController/WebService.py
+++ src/lumos/LedController/WebService.py
@@ -2,15 +2,38 @@ import tornado
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.web import Application
+from tornado.web import RequestHandler
 from src.lumos.LedController import led_controller
 import logging
+import json
 
 logger = logging.getLogger("led_controller")
+
+
+class ListenerRequestHandler(RequestHandler):
+
+    def post(self):
+        logger.info("WebService: received a POST request in Listener Request endpoint. Processing...")
+
+        request_success = False
+        try:
+            request_data = json.loads(self.request.body)
+        except:
+            logger.error("WebService: could not fetch data from request body")
+            request_success = False
+
+        request_success = led_controller.interpret_request(request_data)
+
+        if request_success:
+            logger.info("WebService: the POST request received in Listener Request endpoint was done successfully")
+        else:
+            logger.info("WebService: the POST request received in Listener Request endpoint was done unsuccessfully")
 
 class WebService():
 
     def __init__(self, port=8000):
         self._app = tornado.web.Application([
+            (r"/listener_request", ListenerRequestHandler)
         ])
         self._port=port
 
