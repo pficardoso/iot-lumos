1b7b58167ac4f4600af686662f06f0d82aa1f70d
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-27 22:47:13 +0100

    ActionListener: created initial structure of ActionListener base class - closes #17

---
+++ src/lumos/ActionListener/ActionListener.py
@@ -0,0 +1,40 @@
+import logging
+import lumos.logger
+
+logger = logging.getLogger("action_listener")
+
+class ActionListener():
+    """"""
+
+    def __init__(self,):
+        """Constructor for ActionListener"""
+        self.id = None
+        self.type = None
+        self.led_controller_ip = None
+        self.led_controller_port = None
+        self.configured = False
+
+    """
+    Setters/Loaders
+    """
+
+    """
+    Getters
+    """
+
+    """
+    Workers
+    """
+
+    """
+    Boolean methods
+    """
+
+    """
+    Checkers
+    """
+
+    """
+    Util methods / Static methods
+    """
+
