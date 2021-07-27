8207d71a06581cddbb85ca8e690c1a40be0113d0
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-27 23:02:33 +0100

    ActionListener: defined base class of Action Listener as a abstract class; implemented class method config() - closes #15

--- src/lumos/ActionListener/ActionListener.py
+++ src/lumos/ActionListener/ActionListener.py
@@ -1,9 +1,10 @@
+import abc
 import logging
 import lumos.logger
 
 logger = logging.getLogger("action_listener")
 
-class ActionListener():
+class ActionListener(metaclass=abc.ABCMeta):
     """"""
 
     def __init__(self,):
@@ -17,7 +18,19 @@ class ActionListener():
     """
     Setters/Loaders
     """
+    def config(self, config_path) -> bool:
+        is_configured_gen = self._config_general(config_path)
+        is_configured_spe = self._config_specialized(config_path)
+        self.configured = (is_configured_gen and is_configured_spe)
+        return
 
+    def _config_general(self, config_path) -> bool:
+        #TODO
+        pass
+
+    @abc.abstractmethod
+    def _config_specialized(self, config_path):
+        pass
     """
     Getters
     """
