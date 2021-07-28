54f95699255247516bb566d197d56d0b07c737e4
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-07-28 22:44:18 +0100

    ActionListener base class: implemented method _config_general() - closes #18

--- src/lumos/ActionListener/ActionListener.py
+++ src/lumos/ActionListener/ActionListener.py
@@ -1,11 +1,15 @@
 import abc
 import logging
+import json
 import lumos.logger
+from src.lumos.ActionListener.ConfigChecker import ConfigChecker
 
 logger = logging.getLogger("action_listener")
 
 class ActionListener(metaclass=abc.ABCMeta):
     """"""
+
+    default_led_controller_port = 8000
 
     def __init__(self,):
         """Constructor for ActionListener"""
@@ -14,22 +18,37 @@ class ActionListener(metaclass=abc.ABCMeta):
         self.led_controller_ip = None
         self.led_controller_port = None
         self.configured = False
+        self._config_checker = ConfigChecker()
 
     """
     Setters/Loaders
     """
     def config(self, config_path) -> bool:
-        is_configured_gen = self._config_general(config_path)
-        is_configured_spe = self._config_specialized(config_path)
-        self.configured = (is_configured_gen and is_configured_spe)
-        return
 
-    def _config_general(self, config_path) -> bool:
-        #TODO
-        pass
+        with open(config_path, "r") as f_conf:
+            config_data = json.load(f_conf)["action_listener"]
+
+        is_configured_gen = self._config_general(config_data)
+        is_configured_spe = self._config_specialized(config_data)
+
+        self.configured = (is_configured_gen and is_configured_spe)
+        return self.configured
+
+    def _config_general(self, config_data:dict) -> bool:
+
+        config_check_flag = self._config_checker.check_config_data(config_data)
+
+        self.id = config_data["id"]
+        self.type = config_data["type"]
+        self.led_controller_port = int(config_data["led_controller_port"]) \
+                                    if "led_controller_port" in config_data.keys() \
+                                    else ActionListener.default_led_controller_port
+        self.led_controller_ip = config_data["led_controller_ip"]
+
+        return config_check_flag
 
     @abc.abstractmethod
-    def _config_specialized(self, config_path):
+    def _config_specialized(self, config_path:dict) -> bool:
         pass
     """
     Getters
