3151b773ee566ff3dd441b4808d4e809f90434f0
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-08-09 22:57:46 +0100

    Improved ConfigChecker class of ActionListeners - it reads the type of ActionListener and runs the necessary methods to check the type-related configs

--- src/lumos/ActionListener/ConfigChecker.py
+++ src/lumos/ActionListener/ConfigChecker.py
@@ -4,22 +4,28 @@
 class ConfigChecker():
 
     _mandatory_fields = ["id", "type", "led_controller_ip"]
+    _mandatory_fields_timer = ["timer_period"]
 
     def __init__(self):
+        self._mandatory_fields=None
         pass
 
-    def check_config_data(self, config_data:dict):
+    def check_config_data(self, config_data:dict, type="base"):
+
+        if type == "base": self.mandatory_fields = ConfigChecker._mandatory_fields
+        elif type == "TimerActionListener": self.mandatory_fields = ConfigChecker._mandatory_fields_timer
+        else: raise Exception(f"Type {type} does not exist")
 
         mandatory_check = self._check_config_mandatory_fields(config_data)
-        fields_value_types_check = self._check_fields_valus_type(config_data)
+        fields_value_types_check = self._check_fields_value_type(config_data, type)
 
         return (mandatory_check and fields_value_types_check)
 
-    def _check_config_mandatory_fields(self, config_data:dict) -> bool:
+    def _check_config_mandatory_fields(self, config_data:dict, type="base") -> bool:
 
         check_flag = True
         fields_not_exist = set()
-        for field in ConfigChecker._mandatory_fields:
+        for field in self.mandatory_fields:
             if field not in config_data.keys():
                 fields_not_exist.add(field)
                 check_flag = False
@@ -30,7 +36,15 @@ class ConfigChecker():
 
         return check_flag
 
-    def _check_fields_valus_type(self, config_data:dict) -> bool:
+    def _check_fields_value_type(self, config_data:dict, type="base") -> bool:
+        if type == "base":
+            return self._check_fields_value_type_base(config_data)
+        elif type == "TimerActionListener":
+            return self._check_fields_value_type_timer(config_data)
+        else:
+            raise Exception(f"Type {type} does not exist")
+
+    def _check_fields_value_type_base(self, config_data:dict) -> bool:
 
         check_flag = True
         if "led_controller_port" in config_data:
@@ -42,6 +56,14 @@ class ConfigChecker():
 
         return check_flag
 
+    def _check_fields_value_type_timer(self, config_data:dict) -> bool:
+        check_flag = True
+        if "timer_period" in config_data:
+            try:
+                value = int(config_data["timer_period"])
+            except ValueError:
+                check_flag = False
+                raise Exception("The value of timer_period should be an int")
 
-
+        return check_flag
 
