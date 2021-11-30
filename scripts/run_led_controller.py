ac08f7c42236429d7b3e462007701bb8b9f227bb
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-11-30 00:38:54 +0000

    Created script scripts/run_led_controller.py - closes #27

---
+++ scripts/run_led_controller.py
@@ -0,0 +1,9 @@
+from src.lumos.LedController import start_led_controller_web_service
+from argparse import ArgumentParser
+import os
+
+parser = ArgumentParser()
+parser.add_argument("-c","--config", help="Config file of led controller")
+
+args = parser.parse_args()
+start_led_controller_web_service(args.config)
