d209fe7567eb4cfaa1ab8aade6f6babff5f15be6
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-11-30 17:56:03 +0000

    Created class ActionListerCatalog, which instantiate an ActionListener by Type

--- src/lumos/ActionListener/__init__.py
+++ src/lumos/ActionListener/__init__.py
@@ -1,1 +1,13 @@
 from src.lumos.ActionListener.Timer import Timer
+from src.lumos.ActionListener.ActionListener import ActionListener
+
+class ActionListerCatalog():
+
+    def __init__(self):
+        self._catalog_dict = dict()
+        self._catalog_dict = {Timer.type: Timer}
+
+    def get_action_listener(self, type:str) -> ActionListener:
+        return self._catalog_dict[type]()
+
+
