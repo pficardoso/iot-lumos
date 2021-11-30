3e09309169757cbb32e8684fef6367b8958b6cfb
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-11-30 17:57:36 +0000

    Created script run_action_listener.py - closes #28

---
+++ scripts/run_action_listener.py
@@ -0,0 +1,11 @@
+from src.lumos.ActionListener import ActionListerCatalog
+from argparse import ArgumentParser
+import os
+
+parser = ArgumentParser()
+parser.add_argument("-c","--config", help="Config file of led controller")
+
+args = parser.parse_args()
+action_listener = ActionListerCatalog().get_action_listener("Timer")
+action_listener.config(args.config)
+action_listener.start()
