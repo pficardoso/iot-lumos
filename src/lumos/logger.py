27a2f9485d446f75340e399a2b720e91eb82f35f
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-11-30 00:35:59 +0000

    src/lumos/logger.py and configs/logging.conf where changed in order to ensure that logs are always written in the same directory, independently where files are ran

--- src/lumos/logger.py
+++ src/lumos/logger.py
@@ -1,8 +1,10 @@
 import logging.config
+from src.lumos.definitions import CONF_DIR, LOG_DIR
 import os
 
+os.chdir(LOG_DIR)  ## import to ensure that logs are written in LOG_DIR
 
-conf_file = "configs/logging.conf"
+conf_file = "/".join([CONF_DIR, "logging.conf"])
 if not os.path.exists(conf_file):
     raise Exception(f"File {os.path.abspath(conf_file)} does not exist")
 
