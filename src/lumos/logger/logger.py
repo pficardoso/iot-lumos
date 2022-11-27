cea3f2e85ef7bc2be066485794786471f2bbc2da
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2022-11-27 17:49:17 +0000

    Closes #47 - Improved logging code

--- src/lumos/logger/logger.py
+++ src/lumos/logger/logger.py
@@ -5,7 +5,7 @@ import logging.config
 definitions = Definitions()
 
 long_conf_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logging.conf")
-os.chdir(definitions.log_dir)  ## import to ensure that logs are written in LOG_DIR
+os.makedirs(definitions.log_dir, exist_ok=True)
 
 if not os.path.exists(long_conf_file):
     raise Exception(f"File {os.path.abspath(long_conf_file)} does not exist")
