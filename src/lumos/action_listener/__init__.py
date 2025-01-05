cd14ac1b944eddaf691d065a23206345e2339701
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2025-01-05 22:16:46 +0000

    Deprecated Hand Clap Detector action listener

--- src/lumos/action_listener/__init__.py
+++ src/lumos/action_listener/__init__.py
@@ -1,5 +1,4 @@
 from lumos.action_listener.action_listener import ActionListener
-from lumos.action_listener.hand_clap_detector import HandClapDetector
 from lumos.action_listener.timer import Timer
 
 
@@ -8,7 +7,6 @@ class ActionListerCatalog:
         self._catalog_dict = dict()
         self._catalog_dict = {
             Timer.type: Timer,
-            HandClapDetector.type: HandClapDetector,
         }
 
     def get_action_listener(self, type: str) -> ActionListener:
