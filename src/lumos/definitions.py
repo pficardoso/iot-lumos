ee490e01d3bb18eed8c9490e8b443144d14d80f5
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2025-02-02 13:51:02 +0000

    Add to Definitions mapping between color names and rgb coordinates

--- src/lumos/definitions.py
+++ src/lumos/definitions.py
@@ -27,3 +27,19 @@ class Definitions(metaclass=SingletonMeta):
         self.models_dir = os.path.join(self.root_dir, "models")
 
         self.led_brigthness_step = int(255 / 10)
+        self.colors_rgb_map = {
+            "red": (255, 0, 0),
+            "green": (0, 255, 0),
+            "blue": (0, 0, 255),
+            "yellow": (255, 255, 0),
+            "orange": (255, 165, 0),
+            "purple": (128, 0, 128),
+            "pink": (255, 192, 203),
+            "black": (0, 0, 0),
+            "white": (255, 255, 255),
+            "gray": (128, 128, 128),
+            "brown": (165, 42, 42),
+            "turquoise": (64, 224, 208),
+            "silver": (192, 192, 192),
+            "gold": (218, 165, 32),
+        }
